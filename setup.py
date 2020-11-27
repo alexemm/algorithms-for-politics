b47afd17f277be5da2446385b59f464211348614
Alexander Emmerich <al_emmerich@gmx.de>
2020-11-27 12:42:34 +0100

    Added requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PolAlgo',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/alexemm/condorcet',
+    license='',
+    author='Alexander Emmerich',
+    author_email='al_emmerich@gmx.de',
+    description='Algorithms which are used for political calculations'
+)
