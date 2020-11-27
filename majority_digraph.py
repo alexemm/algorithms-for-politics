562962fc589e623b284f6955ad0e50d91bd7da47
Alexander Emmerich <al_emmerich@gmx.de>
2020-11-27 10:46:22 +0100

    Finished Majority Digraph creation and implemented plotting

--- majority_digraph.py
+++ majority_digraph.py
@@ -1,5 +1,8 @@
 from typing import Optional, List, Dict, Set, Tuple
 from itertools import product
+from networkx import DiGraph, spring_layout, draw_networkx, get_edge_attributes, draw_networkx_edge_labels
+
+import matplotlib.pyplot as plt
 
 
 class Edge:
@@ -7,17 +10,65 @@ class Edge:
     def __init__(self, source: str, destination: str, weight: Optional[int] = None):
         self.source: str = source
         self.dest: str = destination
-        self.weight: Optional[str] = weight
+        self.weight: Optional[int] = weight
+
+    def add_weight(self, n: int):
+        if self.weight is not None:
+            self.weight += n
+
+    def __eq__(self, other):
+        return self.source == other.source and self.dest == other.dest
+
+    def __hash__(self):
+        return hash((self.source, self.dest))
 
 
 class Edges:
 
-    def __init__(self, edges: List[Edge]):
-        # TODO: maybe list of tuples?
-        self.edges: List[Edge] = edges
+    def __init__(self, edges: List[Tuple[str, str]]):
+        self.edges: Set[Edge] = set([Edge(src, dest, 0) for src, dest in edges])
 
     def add_weight(self, source: str, dest: str):
-        pass
+        self[(source, dest)].add_weight(1)
+        self[(dest, source)].add_weight(-1)
+
+    def __getitem__(self, key: Tuple[str, str]) -> Optional[Edge]:
+        if not isinstance(key, tuple):
+            raise TypeError("Your key is not a tuple")
+        if len(key) != 2:
+            raise TypeError("Your key is not a tuple of a source and destination")
+        src, dest = key
+        for edge in self.edges:
+            if edge.source == src and edge.dest == dest:
+                return edge
+        raise KeyError("Your key does not exist in this container")
+
+    def __len__(self):
+        return len(self.edges)
+
+
+class MajorityDigraph:
+
+    def __init__(self, profile: Dict[str, List[str]]):
+        self.alternatives, self.edges = create_majority_digraph(profile)
+        self.edges = [edge for edge in self.edges.edges if edge.weight > 0]
+
+    def plot(self, weighted: bool = False, save_file: str = ""):
+        g: DiGraph = DiGraph()
+        for edge in self.edges:
+            if weighted:
+                g.add_edge(edge.source, edge.dest, weight=edge.weight)
+            else:
+                g.add_edge(edge.source, edge.dest, weight=None)
+        pos = spring_layout(g)
+        draw_networkx(g, pos, node_color='w')
+        if weighted:
+            labels = get_edge_attributes(g, 'weight')
+            draw_networkx_edge_labels(g, pos, edge_labels=labels)
+        plt.axis("off")
+        if save_file != "":
+            plt.savefig(save_file)
+        plt.show()
 
 
 def determine_alternatives(profile: Dict[str, List[str]]) -> Set[str]:
@@ -42,21 +93,22 @@ def get_edge_tuples(alternatives: Set[str]):
     """
     # return (list(i) for i in product(alternatives, repeat=2)
     #        if tuple(reversed(i)) >= tuple(i) and i[0] != i[1])
-    return (list(i) for i in product(alternatives, repeat=2) if i[0] != i[1])
+    return (tuple(i) for i in product(alternatives, repeat=2) if i[0] != i[1])
 
 
-def create_majority_digraph(profile: Dict[str, List[str]]) -> Edges:
+def create_majority_digraph(profile: Dict[str, List[str]]) -> Tuple[Set[str], Edges]:
     # TODO: finish dis
     alternatives: Set[str] = determine_alternatives(profile)
-    edge_list: List[str] = get_edge_tuples(alternatives)
+    edge_list: List[Tuple[str, str]] = list(get_edge_tuples(alternatives))
+
     edges: Edges = Edges(edge_list)
     for voter, ballot in profile.items():
-        print("Voter: "+voter)
+        #print("Voter: "+voter)
         for winner, loser in generate_duels(ballot):
-            print(winner + " wins against " + loser)
+            #print(winner + " wins against " + loser)
             edges.add_weight(winner, loser)
-        print()
-    return edges
+        #print()
+    return alternatives, edges
 
 
 def generate_duels(ballot: List[str]):
@@ -71,5 +123,6 @@ def generate_duels(ballot: List[str]):
                 yield winner, loser
 
 
-def plot_majority_digraph(profile: List[str], weighted: bool = False, save_loc: str = ''):
-    pass
+def plot_majority_digraph(profile: Dict[str, List[str]], weighted: bool = False, save_loc: str = ''):
+    mj = MajorityDigraph(profile)
+    mj.plot(weighted, save_loc)
--- test.py
+++ test.py
@@ -1,16 +1,20 @@
-from majority_digraph import get_edge_tuples, determine_alternatives, create_majority_digraph
+from majority_digraph import MajorityDigraph
 from utils import read_file
 
 
 def test():
-    file_name = 'profiles.json'
+    file_name = 'profiles/profiles.json'
     profile = read_file(file_name)
-    print(profile)
-    alternatives = determine_alternatives(profile)
-    print(alternatives)
-    edges = get_edge_tuples(alternatives)
-    print(list(edges))
-    create_majority_digraph(profile)
+    # print(profile)
+    # alternatives = determine_alternatives(profile)
+    # print(alternatives)
+    # edges = get_edge_tuples(alternatives)
+    # print(list(edges))
+    save_file = "plots/0.png"
+    save_file_weighted = "plots/1.png"
+    mj = MajorityDigraph(profile)
+    mj.plot(False, save_file)
+    mj.plot(True, save_file_weighted)
 
 
 test()
